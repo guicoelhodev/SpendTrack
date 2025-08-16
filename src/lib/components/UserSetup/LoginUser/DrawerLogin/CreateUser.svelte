c1659404ae6317822d44765990ff5cbff08e1c1e
guicoelhodev <gs.coelho_dev@outlook.com>
2025-08-16 11:48:06 -0300

    feat(zod): add zod validation to prevent create users errors

--- src/lib/components/UserSetup/LoginUser/DrawerLogin/CreateUser.svelte
+++ src/lib/components/UserSetup/LoginUser/DrawerLogin/CreateUser.svelte
@@ -1,21 +1,53 @@
 <script lang="ts">
 	import type { TUser } from "$lib/types/User";
+	import * as z from 'zod'
+
+	const formShema = z.object({
+		name: z
+			.string()
+			.nonempty({ message: 'Required field'})
+			.refine(i => i.trim().split(' ').length === 2, {
+				message: 'Add name and last name'
+			}),
+		nickname: z
+			.string()
+			.nonempty({ message: 'Required field'})
+			.refine(i => i.split(' ').length === 1, {
+				message: 'Choose a unique username, like myUser or my_user.'
+			})
+		,
+	});
+
+	let formData: TUser = $state({
+		name: '',
+		nickname: ''
+	});
+
+	let formErrors: Record<keyof TUser, string> = $state({
+		name: '',
+		nickname: ''
+	})
 
 	type TCreateUserProps = {
 		onCreateUser: (newUser: TUser) => void 
 	}
 
 	const props: TCreateUserProps = $props()
-
 </script>
 <form 
-	onsubmit={(e) => {
+	onsubmit={async(e) => {
 		e.preventDefault();
-		
-		const formData = new FormData(e.target as HTMLFormElement);
-		const data = Object.fromEntries(formData.entries()) as TUser
+		const validation = await formShema.safeParseAsync(formData);
 
-		return props.onCreateUser(data)
+		if(validation.success) return props.onCreateUser(formData)
+
+		Object.keys(formData).forEach(inputKey => {
+			const key = inputKey as keyof TUser
+			const error = validation.error.issues.find(i => i.path[0] === inputKey)
+
+			if(error?.message) return formErrors[key] = error.message
+			return formErrors[key] = ''
+		})
 	}}
 	class="flex flex-col gap-4 h-full justify-between">
 	<section class="flex flex-col gap-2">
@@ -28,12 +60,16 @@
 			</label>
 
 			<input 
-				defaultValue='Hello World'
+				bind:value={formData.name}
 				name='name'
 				id='name-input'
 				placeholder="Type here your name"
 				class="border p-2 rounded-md border-text-secondary focus:outline-1  focus:outline-text-primary"
 			/>
+
+			{#if formErrors.name}
+				<span class="text-sm text-red">{formErrors.name}</span>
+			{/if}
 		</article>
 
 		<article class="flex flex-col gap-2">
@@ -45,12 +81,16 @@
 			</label>
 
 			<input 
-				defaultValue='hello_world'
+				bind:value={formData.nickname}
 				name='nickname'
 				id='nickname-input'
 				placeholder="Type here your username"
 				class="border p-2 rounded-md border-text-secondary focus:outline-1  focus:outline-text-primary"
 			/>
+
+			{#if formErrors.nickname}
+				<span class="text-sm text-red">{formErrors.nickname}</span>
+			{/if}
 		</article>
 	</section>
 	
