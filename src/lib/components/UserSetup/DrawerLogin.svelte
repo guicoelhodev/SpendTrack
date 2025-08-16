9e28677b4d294054acd07bf664737c942d3d8f97
guicoelhodev <gs.coelho_dev@outlook.com>
2025-08-16 01:37:44 -0300

    feat: create Drawer component

---
+++ src/lib/components/UserSetup/DrawerLogin.svelte
@@ -0,0 +1,16 @@
+<div class="drawer w-[360px] h-svh fixed top-0 right-0 bg-red-400">
+opa
+</div>
+
+<style>
+
+	.drawer {
+		animation: show 400ms linear
+	}
+
+	@keyframes show {
+		from { transform: translateX(100%); }
+		to { transform: translateX(0); }
+	}
+</style>
+
--- src/lib/components/UserSetup/LoginUser.svelte
+++ src/lib/components/UserSetup/LoginUser.svelte
@@ -1,8 +1,18 @@
 <script lang="ts">
-	let isLogged = $state(false)
+	import DrawerLogin from "./DrawerLogin.svelte";
 
-	function toggleLogIn(){
-		return isLogged = !isLogged
+	type TActions = {
+		isLogged: boolean;
+		showDrawer: boolean
+	};
+
+	let actions: TActions = $state({
+		isLogged: false,
+		showDrawer: false
+	});
+
+	function handleActions(partialObj: Partial<TActions>){
+		actions = Object.assign(actions, partialObj)
 	};
 
 </script>
@@ -10,9 +20,14 @@
 
 <article 
 	class="wrapper flex gap-4 items-start"
-	data-open={isLogged}
+	data-open={actions.isLogged}
 >
-	{#if isLogged}
+
+	{#if actions.showDrawer}
+		<DrawerLogin />
+	{/if}
+
+	{#if actions.isLogged}
 		<div class="w-64 flex flex-col">
 			<p class="text-sm text-nowrap">Signed as <b>guicoelhodev</b></p>
 			<span class="text-xs text-green-500">Online</span>
@@ -23,12 +38,13 @@
 			<span class="text-xs text-red">Offline</span>
 		</div>
 	{/if}
+
 	<button
-		onclick={toggleLogIn}
+		onclick={() => handleActions({ showDrawer: !actions.showDrawer })}
 		class="bg-background-primary p-2 rounded-full grid place-content-center"
 		aria-labelledby='logout-btn'
 	>
-	{#if isLogged}
+	{#if actions.showDrawer}
 		<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M5 21q-.825 0-1.412-.587T3 19V5q0-.825.588-1.412T5 3h7v2H5v14h7v2zm11-4l-1.375-1.45l2.55-2.55H9v-2h8.175l-2.55-2.55L16 7l5 5z"/></svg>
 	{:else}
 		<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M12 4a4 4 0 0 1 4 4a4 4 0 0 1-4 4a4 4 0 0 1-4-4a4 4 0 0 1 4-4m0 10c4.42 0 8 1.79 8 4v2H4v-2c0-2.21 3.58-4 8-4"/></svg>
