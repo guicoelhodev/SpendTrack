c11da9f5c926ca800b8ddeecd219218cd0270a61
guicoelhodev <gs.coelho_dev@outlook.com>
2025-08-15 23:39:15 -0300

    feat(style): add toggle theme

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,26 @@
+<script lang="ts">
+	let isDark = $state(true);
+
+	function toggleTheme(){
+		if(isDark){
+			document.documentElement.classList.remove('dark')
+		} else {
+			document.documentElement.classList.add('dark')
+		}
+
+		return isDark = !isDark
+	};
+
+</script>
+<div class="bg-background-primary w-svw h-svh text-white p-4">
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+	<div class="bg-background-secondary w-36 h-36">
+
+	</div>
+
+	<button onclick={toggleTheme}>
+		mudar
+	</button>
+</div>
