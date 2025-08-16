f2145b84fa32493ff345b2308e336afe67f19b75
guicoelhodev <gs.coelho_dev@outlook.com>
2025-08-16 20:18:29 -0300

    feat(UI): create Modal

---
+++ src/lib/components/ui/Modal.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+	import type { Snippet } from "svelte";
+
+	type TProps = {
+		title: string
+		onClose: VoidFunction
+		children: Snippet
+	}
+	const props: TProps = $props()
+</script>
+
+<div class="fixed top-0 left-0 w-svw h-svh backdrop-blur-md z-30 grid place-content-center">
+
+	<section class="wrapper max-h-[calc(100svh-5rem)] max-w-[calc(100svw-5rem)] h-auto overflow-auto bg-background-secondary p-4 rounded-md flex flex-col gap-4">
+		
+		<header class="flex justify-between gap-4 items-center">
+			<h3 class="text-lg font-semibold">{props.title}</h3>
+			<button aria-labelledby="close-modal" onclick={props.onClose}>
+				<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="m12 13.4l-4.9 4.9q-.275.275-.7.275t-.7-.275t-.275-.7t.275-.7l4.9-4.9l-4.9-4.9q-.275-.275-.275-.7t.275-.7t.7-.275t.7.275l4.9 4.9l4.9-4.9q.275-.275.7-.275t.7.275t.275.7t-.275.7L13.4 12l4.9 4.9q.275.275.275.7t-.275.7t-.7.275t-.7-.275z"/></svg>
+			</button>
+		</header>
+
+		{@render props.children()}
+	</section>
+</div>
+
+<style>
+	.wrapper {
+		animation: open 300ms ease
+	}
+
+	@keyframes open {
+		0% { scale: 0.5 }
+		100% { scale: 1.00 }
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,17 +4,31 @@
 	import MonthlyExpenseChart from "$lib/components/core/MonthlyExpenseChart/index.svelte";
 	import MonthlyExpenseCategory from "$lib/components/core/MonthlyExpenseCategory/index.svelte";
 	import YearlyExpenseChart from "$lib/components/core/YearlyExpenseChart/index.svelte";
+	import Modal from "$lib/components/ui/Modal.svelte";
 
+	let showModal = $state(false);
 </script>
 
 <div class="relative transition-colors bg-background-primary text-text-primary h-svh w-svw">
+
+	{#if showModal}
+		<Modal title='Setup modal' onClose={() => showModal = false }>
+			<section class="min-w-[600px]">
+				AOBA
+			</section>
+		</Modal>
+	
+	{/if}
 
 	<main class="w-full h-[calc(100%-5rem)] grid grid-cols-2 gap-4 p-4">
 
 		<article class="col-span-2 flex flex-col justify-evenly border-2 border-background-secondary rounded-md">
 			<div class="p-4">
 				<p class="text-3xl font-semibold text-red">TOTAL: R$ 2350,00</p>
+
+				<button onclick={() => showModal = true } class="p-2 bg-background-secondary">ABRIR MODAL</button>
 			</div>
+
 
 			<MonthlyExpenseChart />
 		</article>
