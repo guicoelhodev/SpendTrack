653646a751d926115fddc96b4e556c2adb72b313
guicoelhodev <gs.coelho_dev@outlook.com>
2025-08-16 22:05:13 -0300

    feat(SetupModal|About): create About session

---
+++ src/lib/components/core/UserSetup/UserSetup/SetupModal/About.svelte
@@ -0,0 +1,23 @@
+<section class="flex flex-col gap-2 text-text-secondary">
+	<h3 class="text-lg font-semibold text-text-primary">
+		Spend Track
+	</h3>
+
+	<p>
+		This project runs entirely in your browser. We use <b>IndexedDB</b> to store all your actions locally. 
+	</p>
+
+	<p>We do not collect or access any of your financial information.</p>
+
+	<p>If you want to see the codebase, please take a look at GitHub.</p>
+
+	<aside>
+		<a
+			href='https://github.com/guicoelhodev/SpendTrack'
+			class="underline text-text-primary flex items-center gap-2"
+			target="_blank"
+		>
+			codebase project
+		</a>
+	</aside>
+</section>
--- src/lib/components/core/UserSetup/UserSetup/SetupModal/index.svelte
+++ src/lib/components/core/UserSetup/UserSetup/SetupModal/index.svelte
@@ -5,28 +5,37 @@
 	import SolarInfoCircleOutline from '~icons/solar/info-circle-outline';
 
 	import { application } from "$lib/stores/application.svelte";
+	import type { Component } from "svelte";
+	import About from "./About.svelte";
 
-	type Menu = { icon: any, name: string }
-	const menuOptions: Menu[] = [
-		{ icon: SolarSettingsBold, name: 'General settings'},
-		{ icon: SolarLinkMinimalistic2Bold, name: 'Appearence' },
-		{ icon: SolarInfoCircleOutline, name: 'About project' }
+	type TPaneKey = 'general' | 'appearence' | 'about'
+	type TMenu = { icon: Component, name: string, key: TPaneKey }
+
+	const menuOptions: TMenu[] = [
+		{ icon: SolarSettingsBold, name: 'General settings', key: 'general' },
+		{ icon: SolarLinkMinimalistic2Bold, name: 'Appearence', key: 'appearence' },
+		{ icon: SolarInfoCircleOutline, name: 'About project', key: 'about' }
 	]
 
-	let currentPane = $state<Menu>(menuOptions[0]);
+	let currentPane = $state<TPaneKey>(menuOptions[0].key);
 
+	const paneViews: Record<TPaneKey, Component | null> = {
+		about: About,
+		appearence: null,
+		general: null
+	}
 </script>
 
 <Modal title='Application preferences' onClose={() => application.isOpenSetup = false}>
-	<div class="min-w-[600px] flex divide-x-1 divide-text-secondary">
+	<div class="w-[600px] flex divide-x-1 divide-text-secondary">
 		<ul class="pr-4 flex flex-col">
 			{#each menuOptions as option}
 				{@const Icon = option.icon}
 				<button 
-					class="transition-all flex gap-2 items-center p-2 text-text-secondary rounded-md"
-					class:text-text-primary={currentPane.name === option.name}
-					class:text-text-secondary={currentPane.name !== option.name}
-					onclick={() => currentPane = option}
+					class="transition-all flex gap-2 items-center p-2 text-text-secondary rounded-md text-nowrap"
+					class:text-text-primary={currentPane === option.key}
+					class:text-text-secondary={currentPane !== option.key}
+					onclick={() => currentPane = option.key}
 				>
 					<Icon />
 					{option.name}
@@ -34,8 +43,11 @@
 			{/each}
 		</ul>
 
-		<section class="flex-1 pl-4">
-			{currentPane.name}
+		<section class="w-full pl-4">
+			{#if currentPane}
+				{@const Component = paneViews[currentPane]}
+				<Component />
+			{/if}
 		</section>
 	</div>
 </Modal>
