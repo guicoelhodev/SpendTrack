365dbcd1cb191cd60576fa7c8d7f406b0dff7a3d
guicoelhodev <gs.coelho_dev@outlook.com>
2025-08-18 19:19:45 -0300

    feat: add expanseAmount table indexed

--- src/lib/api/adapters/driven/datasource/db.ts
+++ src/lib/api/adapters/driven/datasource/db.ts
@@ -1,3 +1,4 @@
+import type { ExpanseAmount } from '$lib/api/core/models/ExpanseAmount';
 import type { TExpanseCategory } from '$lib/api/core/models/ExpanseCategory';
 import type { TSession } from '$lib/api/core/models/Session';
 import type { TUser } from '$lib/api/core/models/User';
@@ -11,13 +12,15 @@ const db = new Dexie('SpendTrack_DB') as Dexie & {
   >;
 	session: EntityTable<TSession, 'id'>
 	expanseCategory: EntityTable<TExpanseCategory, 'id'>
+	expanseAmount: EntityTable<ExpanseAmount, 'id'>
 };
 
 // Schema declaration:
 db.version(1).stores({
   users: 'id, name, nickname',
 	session: "++id, userId",
-	expanseCategory: "++id, &name"
+	expanseCategory: "++id, &name",
+	expanseAmount: "++id, createdAt, mountIndex, categoryName"
 });
 
 db.on('populate', async (tx) => {
@@ -27,6 +30,10 @@ db.on('populate', async (tx) => {
     { name: 'transport', hexColor: '#457b9d', isDefault: true  },
     { name: 'fix', hexColor: '#43aa8b', isDefault: true  },
   ]);
+
+	await tx.table('users').add({ 
+		id: crypto.randomUUID(), name: 'Guilherme Coelho', nickname: 'guicoelhodev' 
+	})
 });
 
 
