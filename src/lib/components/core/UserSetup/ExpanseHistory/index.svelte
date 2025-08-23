9f4c46098b6ce1280e6bef49cf33df5a26581a3e
guicoelhodev <gs.coelho_dev@outlook.com>
2025-08-23 13:20:14 -0300

    feat: create ExpanseHistory component

---
+++ src/lib/components/core/UserSetup/ExpanseHistory/index.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import SolarHistoryBold from '~icons/solar/history-bold';
+</script>
+
+<button title="View history">
+	<SolarHistoryBold />
+</button>
--- src/lib/components/core/UserSetup/index.svelte
+++ src/lib/components/core/UserSetup/index.svelte
@@ -1,12 +1,14 @@
 <script lang="ts">
 	import LoginUser from "./LoginUser/index.svelte";
 	import UserSetup from  "./UserSetup/index.svelte";
+	import ExpanseHistory from "./ExpanseHistory/index.svelte";
 </script>
 <section class="flex items-center justify-center h-20">
 	<div
 		class="border-2 border-background-secondary rounded-md flex justify-between items-center gap-2 size-full p-2 sm:h-auto sm:w-auto"
 	>
 		<LoginUser />
+		<ExpanseHistory />
 		<UserSetup />
 	</div>
 </section>
