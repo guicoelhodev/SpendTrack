4a7a99db9f2e2b2984cb0e62845aeaa78a3b0a58
guicoelhodev <gs.coelho_dev@outlook.com>
2025-08-23 13:48:45 -0300

    feat(HistoryExpanse): create HistoryExpanse

--- src/lib/components/core/UserSetup/ExpanseHistory/index.svelte
+++ src/lib/components/core/UserSetup/ExpanseHistory/index.svelte
@@ -1,7 +1,18 @@
 <script lang="ts">
+	import Modal from '$lib/components/ui/Modal.svelte';
 	import SolarHistoryBold from '~icons/solar/history-bold';
+
+	let showModal = $state(true);
 </script>
 
-<button title="View history">
+<button onclick={() => showModal = true } title="View history">
 	<SolarHistoryBold />
 </button>
+
+{#if showModal}
+	<Modal title="History expanse" onClose={() => showModal = false}>
+		<div class="w-[calc(100svh-2rem)] max-w-[40rem]">
+
+		</div>
+	</Modal>
+{/if}
